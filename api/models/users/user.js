c2ae3ae93e9c1eaed58e2d462445b77d3dc0876e
Shlomi Atzmon <shlomiatzmon82u@gmail.com>
2018-07-31 20:05:30 +0300

    adding user prop to token

--- api/models/users/user.js
+++ api/models/users/user.js
@@ -19,8 +19,12 @@ userSchema.methods.generateAuthToken = function() {
   const token = jwt.sign(
     {
       _id: this._id,
+      name:this.name,
+      email:this.email,
       exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
-      isAdmin:this.isAdmin
+      isAdmin:this.isAdmin,
+      avatar:this.avatar,
+      phone:this.phone
     },
     config.get('jwtPrivateKey')
   );

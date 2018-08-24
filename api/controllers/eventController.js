f4564c38535f8dab7cf3d3a99711d5cc78034531
Shlomi Atzmon <shlomiatzmon82u@gmail.com>
2018-08-24 21:30:51 +0300

    public read to uploaded image

--- api/controllers/eventController.js
+++ api/controllers/eventController.js
@@ -105,7 +105,8 @@ exports.uploadEventImage = async (req,res,next) => {
     const file = req.file.location;
     if(!file) return res.status(404).json({error: 'Upload image not succeeded'});
     return res.status(200).json({"url":file});
-  }
+}
+
 // Start Update an Event.
 exports.startUpdateEvent = async (req,res,next) => {
   const id = req.body._id;
@@ -131,6 +132,7 @@ exports.startUpdateEvent = async (req,res,next) => {
     Bucket: bucket,
     CopySource: `${bucket}${OLD_KEY}`,
     Key: `${NEW_KEY}`
+    acl: 'public-read',
   })
   .promise()
   .then(() => s3.deleteObject({
